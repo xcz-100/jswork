e48fecd37c487c0599710e088a84da7cb1ac39dd
apple <apple@jd505-19.local>
2019-10-14 11:52:34 +0800

    work5

--- work5/work5.js
+++ work5/work5.js
@@ -1,5 +1,14 @@
 //9x9的空表，请自行编写这段代码
-
+str='<table border="1">'
+for(let i=9;i>0;--i){
+    str += '<tr>'
+    for(var j=9;j>0;--j){
+        str += '<td>&nbsp;</td>'
+    }
+    str +='</tr>'
+}
+str += '</tsble>'
+document.getElementById('table1').innerHTML=str
 
 
 //倒九九乘法空表
@@ -14,4 +23,14 @@ for(let i=9;i>0;--i){
 str += '</tsble>'
 document.getElementById('table2').innerHTML=str
 
-//倒九九乘法表，请自行编写这段代码
+//倒九九乘法表，请自行编写这段代码ß
+for(let i=1;i<10;++i){
+    str += '<tr>'
+    for(var j=1;j<=i;++j){
+        //;拼接单元格
+        str += '<td>' +j + '* ' + i + '=' + (j*j)+'</td>'
+    }
+    str += '</tr>'
+}
+str += '</table>'
+document.getElementById('table3').innerHTML=str
