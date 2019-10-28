036ab9f45797b678d70873f6d4f4ac0a235bc609
apple <apple@jd505-19.local>
2019-10-28 11:58:59 +0800

    work9

---
+++ work9/cascade.js
@@ -0,0 +1,24 @@
+function createOption(obj,date){
+    for (var i in date){
+        var op =new Option(date[i],i);
+        obj.options.add(op);
+    }
+}
+var collegeSelect =document.getElementById('collegeSelect');
+createOption(collegeSelect, colleges)
+
+var majorSelect =document.getElementById('majorSelect')
+collegeSelect.onchange =function(){
+    majorSelect.options.length =0
+    createOption(majorSelect,majors[collegeSelect.value])
+}
+var classSelect =document.getElementById('classSelect')
+collegeSelect.onchange =function(){
+    majorSelect.options.length =0
+    createOption(majorSelect,majors[collegeSelect.value])
+}
+var cclassSelect =document.getElementById('classSelect')
+majorSelect.onchange =function(){
+    classSelect.options.length =0
+    createOption(classSelect, classes[collegeSelect.value][majorSelect.value])
+}
