479708ff4dcf3e2cad5e0af5717f4481774829e3
xcz-100 <995308268@qq.com>
2019-10-31 09:45:16 +0800

    work10

---
+++ work10/index.js
@@ -0,0 +1,33 @@
+function monkey(){
+    let total =document.getElementById("monkeytotal").value
+    let kick =document.getElementById("monkeykick").value
+    total =parseInt(total) && Number(total)
+    kick  =parseInt(kick) &&Number(kick)
+    if(isNaN(total)||isNaN(kick)){
+        alert('请输入数字')
+        return
+    }
+    let monkey=[]
+    for (let i=1;i<=total;i++){
+        monkey.push(i)
+    }
+    let i=0
+    while (monkey.length>1){
+        i++;
+        head =monkey.shift()
+        if(i%kick!=0){
+            monkey.push(head);
+        }
+    }
+    document.getElementById('monkeyking').innerText =monkey[0]
+}
+function stat(){
+    let str =document.getElementById('str').value
+    let obj = {}
+    var str ="q a e w e e z s e r a s r w e e w a"
+    var n=str.split(" ");
+
+
+
+    document.getElementById('result').innerText =JSON.stringify(obj)
+}
