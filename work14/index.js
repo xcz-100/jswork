ba40589951340172556846383a2128d225993b12
xcz-100 <995308268@qq.com>
2019-12-11 21:14:50 +0800

    work15

--- work14/index.js
+++ work14/index.js
@@ -11,10 +11,10 @@ function seckill(){
         m=parseInt((remaining/60)%60);
         s=parseInt(remaining%60);
 
-        d =d<10? '0' + d : d;
-        h = h <10 ? '0' + h :h;
-        m = m <10 ? '0' + m :m;
-        s = s <10 ? '0' +s :s;
+        d = d <10 ? '0' + d : d;
+        h = h <10 ? '0' + h : h;
+        m = m <10 ? '0' + m : m;
+        s = s <10 ? '0' + s : s;
     }else{
         clearInterval(id);
         d =h = m =s = '00';
